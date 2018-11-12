381344392e636fd5213c0b06b5bf811c80ce4a6d
remyjava <44977298+remyjava@users.noreply.github.com>
2018-11-12 22:39:45 +0200

    Create Loginremy.php

---
+++ Loginremy.php
@@ -0,0 +1,26 @@
+<?php
+    $con = mysqli_connect("localhost", "id7827571_cyclos", "root@123", "id7827571_cyclos3");
+    
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    
+    $statement = mysqli_prepare($con, "SELECT * FROM user WHERE username = ? AND password = ?");
+    mysqli_stmt_bind_param($statement, "ss", $username, $password);
+    mysqli_stmt_execute($statement);
+    
+    mysqli_stmt_store_result($statement);
+    mysqli_stmt_bind_result($statement, $userID, $name, $age, $username, $password);
+    
+    $response = array();
+    $response["success"] = false;  
+    
+    while(mysqli_stmt_fetch($statement)){
+        $response["success"] = true;  
+        $response["name"] = $name;
+        $response["age"] = $age;
+        $response["username"] = $username;
+        $response["password"] = $password;
+    }
+    
+    echo json_encode($response);
+?>
